9a994c6c8e371a1e096a55a1d9c70c3d19ef2c29
Florencia Esmoris <florenciaesmoris88@gmail.com>
2022-10-22 01:41:30 -0300

    Cree los componentes principales. El componente card tiene estilos basicos a modo de guia

---
+++ src/components/Card.vue
@@ -0,0 +1,38 @@
+<script></script>
+
+<template>
+  <div class="card">
+    <div class="flex">
+      <h2>categoria</h2>
+      <h2>region</h2>
+    </div>
+    <article>
+      <p>
+        Body Health, la empresa líder en desarrollo, fabricación y
+        comercialización de aparatología estética, por tercer año consecutivo
+        elige a «Pampita Ardohain» como su embajadora. Inspirados por los
+        objetivos de la compañía, que implican llevar salud y belle...
+      </p>
+    </article>
+    <div class="flex">
+      <span>hour</span>
+      <span>sitio web</span>
+    </div>
+  </div>
+</template>
+
+<style>
+.flex {
+  display: flex;
+  justify-content: space-between;
+}
+.card {
+  width: 600px;
+  font-family: sans-serif;
+  display: flex;
+  flex-direction: column;
+  padding: 2rem;
+  border-radius: 25px;
+  background-color: aliceblue;
+}
+</style>
