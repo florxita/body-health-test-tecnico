a89f4e30118b36d547e5652944099ca9ff946b85
Florencia Esmoris <florenciaesmoris88@gmail.com>
2022-10-22 16:47:30 -0300

    Cree las props necesarias para renderizar el componente Card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,22 +1,28 @@
-<script></script>
+<script setup>
+defineProps({
+  id: Number,
+  category: String,
+  region: String,
+  body: String,
+  hour: Date,
+  url: Array,
+});
+</script>
 
 <template>
   <div class="card">
     <div class="flex">
-      <h2>categoria</h2>
-      <h2>region</h2>
+      <h2>{{ category }}</h2>
+      <h2>{{ region }}</h2>
     </div>
     <article>
       <p>
-        Body Health, la empresa líder en desarrollo, fabricación y
-        comercialización de aparatología estética, por tercer año consecutivo
-        elige a «Pampita Ardohain» como su embajadora. Inspirados por los
-        objetivos de la compañía, que implican llevar salud y belle...
+        {{ body }}
       </p>
     </article>
     <div class="flex">
-      <span>hour</span>
-      <span>sitio web</span>
+      <span>{{ hour }}</span>
+      <span>{{ url }}</span>
     </div>
   </div>
 </template>
