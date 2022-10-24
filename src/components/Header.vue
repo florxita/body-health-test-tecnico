e060b76a6014db7c8ef4c314746a63594c41c0c9
Florencia Esmoris <florenciaesmoris88@gmail.com>
2022-10-24 02:18:55 -0300

    Cree el buscador y traje la data mediante props, al intentar recorrerla en script no muestra nada, aunque si puedo acceder a la info en el template

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref, computed } from "vue";
+
+const { news } = defineProps({
+  news: Object,
+});
+
+const search = ref(""); // input del user
+// Estaba intentando filtrar la info de news traida como props desde el padre pero no funciona
+// const filtroCategorias = news.value.filter((_new) => _new.category == "tendencias");
+
+// console.log(filtroCategorias);
+</script>
+<template>
+  <section class="header">
+    <h1>Stetic News</h1>
+
+    <input type="search" id="search" placeholder="Search" v-model="search" />
+    <button @click="buscarEnCategoria">Buscar</button>
+    <template v-for="_new in news" :key="_new.id">
+      <!-- {{ _new.category }} -->
+    </template>
+  </section>
+</template>
